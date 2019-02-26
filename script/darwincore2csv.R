ef0cceacc7677e7cb5791e6934adfaa87cf74a66
Rutger Vos <rutgeraldo@gmail.com>
2019-02-27 00:48:52 +0100

    styler

--- script/darwincore2csv.R
+++ script/darwincore2csv.R
@@ -5,39 +5,39 @@ library(finch) # reads darwincore zip file
 library(dplyr) # data munging library
 
 # what to write in the data file and output csv file name
-genus.name   <- 'Camelus'
-species.name <- 'bactrianus'
+genus.name <- "Camelus"
+species.name <- "bactrianus"
 
-# for the workflow of unpacking a zip file downloaded from gbif into the 
+# for the workflow of unpacking a zip file downloaded from gbif into the
 # /data/domesticated folder these variables can be left unchanged
-REPO_HOME    <- paste(getwd(), '/../', sep = '')
-infile.name  <- sprintf("%s/data/domesticated/%s_%s/darwincore.zip", REPO_HOME, genus.name, species.name)
+REPO_HOME <- paste(getwd(), "/../", sep = "")
+infile.name <- sprintf("%s/data/domesticated/%s_%s/darwincore.zip", REPO_HOME, genus.name, species.name)
 outfile.name <- sprintf("%s/data/filtered/%s_%s.csv", REPO_HOME, genus.name, species.name)
 
 # read the occurrences file from darwincore archive
 dwca_cache$delete_all()
 infile.dwca_gbif <- dwca_read(infile.name, read = TRUE)
-occurrences.df   <- infile.dwca_gbif$data$occurrence.txt
+occurrences.df <- infile.dwca_gbif$data$occurrence.txt
 
 # select, filter
 occurrences.df <- select(
-  occurrences.df, 
-  gbif_id = gbifID, 
-  decimal_latitude = decimalLatitude, 
+  occurrences.df,
+  gbif_id = gbifID,
+  decimal_latitude = decimalLatitude,
   decimal_longitude = decimalLongitude
 )
-occurrences.df <- occurrences.df[!duplicated(occurrences.df[2:3]),]
+occurrences.df <- occurrences.df[!duplicated(occurrences.df[2:3]), ]
 
 # add taxon name column
 occurrences.df$taxon_name <- paste(genus.name, species.name)
 
 # reorder columns
-occurrences.df <- occurrences.df[,c(1,4,2,3)]
+occurrences.df <- occurrences.df[, c(1, 4, 2, 3)]
 
 # write out file
 write.csv(
-  occurrences.df, 
-  file = outfile.name, 
+  occurrences.df,
+  file = outfile.name,
   quote = F,
   eol = "\n",
   row.names = F
