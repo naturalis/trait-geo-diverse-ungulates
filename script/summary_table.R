816b57418b6afcf0937494e6443df22db57f6203
ElkeHendrix <elkehendrix115@gmail.com>
2019-04-05 14:43:10 +0200

    added descending variable importance

--- script/summary_table.R
+++ script/summary_table.R
@@ -17,10 +17,16 @@ df.auc<- read.csv(df.auc, header = T)
 
 # load trait contribution
 df.traits<- sprintf("%s/Results/maxent/traits_contribution_maxent.csv", REPO_HOME)
-df.traits <- read.csv(df.traits, header = T)
+df.traits <- read.csv(df.traits, header = T, row.names = 1)
+d<- as.data.frame(names(df.traits))
+write.csv(d, "C:/Users/elkeh/Documents/Stage_Naturalis/summarytable_nummers_variabelen.csv")
+y <- c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15",
+       "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "29",
+       "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41")
+colnames(df.traits) <- y
 
 # make output directories, if needed
-i=2
+
 for (i in 1:length(taxa.names)) {
   # set species name
   df.summary[i,1] <- taxa.names[i]
@@ -33,8 +39,11 @@ for (i in 1:length(taxa.names)) {
   # order the trait contribution
   traits.row <- df.traits[i,]
   traits.row<- na.omit(t(traits.row))
-  traits.row <- traits.row[,order(i)] 
-  
+  traits.row<-traits.row[order(traits.row, decreasing = TRUE),] 
+  traits.row<- as.data.frame(traits.row)
+  traits.row<- rownames(traits.row)
+  df.summary[i,4] <- paste(traits.row, collapse= "," )
   }
-  
 
+output<- paste(REPO_HOME, "/Results/maxent/summary_df.csv", sep="")  
+write.csv(df.summary, output)
